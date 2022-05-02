86892d9a172ca505de9647ec98ae0a71d2fe5a45
nikosge <nikosge2@gmail.com>
2022-05-02 22:40:37 +0300

    feat: add send button with styles

--- client/src/App/Input/index.jsx
+++ client/src/App/Input/index.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 import "./index.css"
 
 const Input = props => {
+  const [input, setInput] = useState('')
+
+  const onChange = e => setInput(e.target.value)
+
   return (
     <div className="Input-root">
-      <input className="Input"/>
+      <input placeholder="Aa" onChange={onChange} value={input} className="Input"/>
+      <button className="Input-button">Send</button>
     </div>
   )
 }

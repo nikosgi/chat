7291c63818a3ddad9f60b8c922fc1809d39a9c3b
nikosge <nikosge2@gmail.com>
2022-05-02 23:55:36 +0300

    feat: add parsing message function

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Messages from './App/Messages'
 import Input from './App/Input';
 import './App.css';
 import { useState } from 'react';
+import parseMessage from './utils/parseMessage';
 
 function App() {
 
@@ -11,12 +12,23 @@ function App() {
     const {action, value} = parseMessage(message)
 
     switch(action){
-      case '/nick': setUser(value); break;
-      case '/oops': deleteLastMessage(); break;
-      case '/think': sendMessage(value,'think'); break;
-      case '/send': sendMessage(value); break;
+      case 'nick': setUser(value); break;
+      case 'oops': deleteLastMessage(); break;
+      case 'think': sendMessage(value,'think'); break;
+      case 'send': sendMessage(value); break;
     }
+  }
 
+  const sendMessage = message => {
+    const payload = {
+      value: message,
+      timestamp: (new Date()).getTime()
+    }
+    setMessages([...messages, payload])
+  }
+
+  const deleteLastMessage = () => {
+    setMessages(messages.shift())
   }
 
   return (
---
+++ client/src/utils/parseMessage.js
@@ -0,0 +1,26 @@
+const AVAILABLE_ACTIONS = [
+  'nick',
+  'think',
+  'oops'
+]
+
+export default message => {
+  const words = message.split(" ")
+  const [first, ...rest] = words;
+  const [firstLetter, ...restLetters] = first;
+  let action = ''
+  let value = ''
+
+  if (firstLetter === '/') {
+    action = restLetters.join('')
+  }
+
+  if (AVAILABLE_ACTIONS.includes(action)) {
+    value = rest[0]
+  } else {
+    action = 'send'
+    value = message
+  }
+
+  return {action, value}
+}

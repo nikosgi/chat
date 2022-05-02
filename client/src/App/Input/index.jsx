25f470ffff2f2426d1fa8e5efee9c3ef0c1fa28a
nikosge <nikosge2@gmail.com>
2022-05-02 23:03:56 +0300

    feat: add keypress enter functionality

--- client/src/App/Input/index.jsx
+++ client/src/App/Input/index.jsx
@@ -4,24 +4,17 @@ import "./index.css"
 const Input = props => {
   const [input, setInput] = useState('')
 
-  useEffect( () => {
-
-  }, )
-
-  const onFocus = () => {
-    window.addEventListener('keypress', handleKeyPress)
-  }
-
-  const onBlur = () => {
-    window.removeEventListener('keypress', handleKeyPress)
-  }
-
-  const handleKeyPress = key => {
-    console.log(key)
-  }
   const handleChange = e => setInput(e.target.value)
-  const sendMessage = e => {
-
+  const onKeyPress = e => {
+    if (e.key === "Enter") {
+      sendMessage();
+    }
+  }
+  const sendMessage = () => {
+    if (input !== ''){
+      console.log('sending')
+      setInput('')
+    }
   }
 
   return (
@@ -29,10 +22,9 @@ const Input = props => {
       <input 
         className="Input"
         placeholder="Aa"
-        value={input}         
+        value={input}
+        onKeyPress={onKeyPress} 
         onChange={handleChange}
-        onFocus={onFocus}
-        onBlur={onBlur}
       />
       <button 
         className="Input-button"

d829da5c26d4430660fa5e4d45f2a0ff962a2475
nikosge <nikosge2@gmail.com>
2022-05-04 03:28:26 +0300

    feat: implement remove user

--- server/users.js
+++ server/users.js
@@ -1,13 +1,21 @@
-const users = []
+const MAX_USERS_PER_ROOM = 2;
 const rooms = [{
-  id: "1", space: 2, active: 0
+  users: []
 }]
 
 export const addUser = user => {
-  users.push(user)
-  if( )
+  const roomIdx = rooms.findIndex(r => r.users.length < MAX_USERS_PER_ROOM)
+  if(roomIdx !== -1){
+    rooms[roomIdx].push(user)
+  }else{
+    rooms.push({
+      users: [user]
+    })
+  }
 }
 
 export const removeUser = user => {
-  users.filter( u => u !== user)
+  const roomIdx = rooms.findIndex(r => r.users.includes(user))
+  const {users} = rooms[roomIdx]
+  rooms[roomIdx].users = users.filter( u => u !== user)
 }

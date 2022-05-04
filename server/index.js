885680bfdbcb40f09a4d6a62e58b8e617fcaa84b
nikosge <nikosge2@gmail.com>
2022-05-04 03:25:05 +0300

    feat: implement add user

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import { Server } from "socket.io";
 const port = process.env.PORT || 8000;
-
 const io = new Server();
+
+const {addUser, removeUser} = "./users.js"
 
 io.on("connection", (socket) => {
   console.log("New connection :", socket)
+  socket.on("join", () => {
+    addUser(socket.id)
+  })
+  socket.on("message", onMessage)
+  socket.on("leave", () => {
+    removeUser(socket.id)
+  })
 });
 
 io.listen(port)
---
+++ server/users.js
@@ -0,0 +1,13 @@
+const users = []
+const rooms = [{
+  id: "1", space: 2, active: 0
+}]
+
+export const addUser = user => {
+  users.push(user)
+  if( )
+}
+
+export const removeUser = user => {
+  users.filter( u => u !== user)
+}
